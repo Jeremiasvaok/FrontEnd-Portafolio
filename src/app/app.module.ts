eed57e28a89025abbca9536cbdfea002c00a9e34
Jeremias Vallejos Gonzalez <jeremias.vallejos78@gmail.com>
2023-04-28 17:48:16 -0300

    porfolio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './Components/nav/nav.component';
+import { PorfolioComponent } from './Components/porfolio/porfolio.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    PorfolioComponent
   ],
   imports: [
     BrowserModule
