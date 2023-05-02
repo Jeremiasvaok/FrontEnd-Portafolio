44b5c419e4fa508cd789c107afd6530994b21ef9
Jeremias Vallejos Gonzalez <jeremias.vallejos78@gmail.com>
2023-05-02 19:28:31 -0300

    save

---
+++ src/app/Components/AcercaDe/acercaDe.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+   selector: 'app-acercaDe',
+   templateUrl:'./acercaDe.component.html',
+   styleUrls:['./acercaDe.component.css'],
+})
+
+export class AcercaDeComponent{
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NavComponent } from './Components/nav/nav.component';
 import { PorfolioComponent } from './Components/porfolio/porfolio.component';
+import { AcercaDeComponent } from './Components/AcercaDe/acercaDe.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    PorfolioComponent
+    PorfolioComponent,
+    AcercaDeComponent
   ],
   imports: [
     BrowserModule
