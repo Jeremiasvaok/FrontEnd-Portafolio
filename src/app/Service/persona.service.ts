43cb31a3519f6ad62adcb4e099837d8a9e7fb418
Jeremias Vallejos Gonzalez <jeremias.vallejos78@gmail.com>
2023-05-15 19:45:02 -0300

    service

--- src/app/Service/persona.service.ts
+++ src/app/Service/persona.service.ts
@@ -1,17 +1,19 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+// import { environment } from 'src/environments/environment';
 import { Persona } from '../Model/persona.model';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class PersonaService {
-Url='http://localhost:8080' 
+  URL = 'localhost:8080/' + 'personas/';
 
   constructor(private http: HttpClient) { }
 
-  public getUser(): Observable<Persona>{
-    return this.http.get<Persona>(this.Url + '/user');
+  public getPersona(): Observable<Persona>{
+    return this.http.get<Persona>(this.URL+ 'traer/perfil');
   }
 }
